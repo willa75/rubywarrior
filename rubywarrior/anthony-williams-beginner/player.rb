8295e792f101d177c6c7d74977ccc21d520cf3f9
KYN 3 <kyn3@KYNs-MacBook-Air.local>
2014-09-02 15:43:02 -0400

    initial commit

---
+++ rubywarrior/anthony-williams-beginner/player.rb
@@ -0,0 +1,28 @@
+class Player
+  def play_turn(warrior)
+    if warrior.health < 19 and warrior.feel.empty? and warrior.health >= @health
+            warrior.rest!
+    else
+        while True
+          if warrior.feel.empty(:backward)
+            warrior.walk!(:backward)
+          else if warrior.feel.captive?(:backward)
+            warrior.rescue!(:backward)
+          else
+            warrior.attack!(:backward)
+          end #ends backwards if statement
+        end#ends while loop  
+
+        if warrior.feel.empty?
+          warrior.walk!
+        else
+          if warrior.feel.captive?
+            warrior.rescue!
+          else
+            warrior.attack!
+          end
+        end#ends 3rd if
+    end#ends the first if
+    @health = warrior.health
+  end#ends the def
+end #ends the class
