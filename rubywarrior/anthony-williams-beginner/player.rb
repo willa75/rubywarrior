970b0437c4c2bae20f3bfacf60c59463b25b75a6
KYN 3 <kyn3@KYNs-MacBook-Air.local>
2014-09-05 09:52:56 -0400

    finished ruby warrior epic mode with score of B

--- rubywarrior/anthony-williams-beginner/player.rb
+++ rubywarrior/anthony-williams-beginner/player.rb
@@ -1,30 +1,66 @@
 class Player
   def play_turn(warrior)
-    if warrior.health < 19 and warrior.feel.empty? and warrior.health >= @health
-            warrior.rest!
-    else
-        do
-          if warrior.feel.empty(:backward)
-            warrior.walk!(:backward)
-          else if warrior.feel.captive?(:backward)
-            warrior.rescue!(:backward)
-          else
-            warrior.attack!(:backward)
-          end#ends else if
-          end #ends backwards if statement
-        break if False   
-        end#ends while loop
+    healer(warrior)
 
-        if warrior.feel.empty?
-          warrior.walk!
-        else
-          if warrior.feel.captive?
-            warrior.rescue!
-          else
-            warrior.attack!
-          end
-        end#ends 3rd if
-    end#ends the first if
+  end #end def play_turn
+
+  def healer(warrior)
+    if warrior.health < 19 && warrior.feel.empty? && warrior.health >= @health #if warrior has taken damage, isn't around an enemy, and isn't taking damage then he uses heal
+      warrior.rest!
+    elsif warrior.health < 4 && warrior.look[2].to_s != "Archer" && warrior.feel(:backward).wall? == false #if warrior takes too much damage then retreat to safety
+      warrior.walk!(:backward)
+    else
+      if warrior.feel.wall? or @forward != 1 #if warrior runs into wall or hasn't tried to go back yet turn around
+        warrior.pivot!
+        @forward = 1
+        $forward = 1
+      elsif
+        range(warrior)
+      end #ends inner-if
+    end #ends if
     @health = warrior.health
-  end#ends the def
-end #ends the class
+  end #ends def healer
+
+  def range(warrior)
+    if warrior.look[0].to_s == "nothing" && warrior.look[1].to_s == "nothing" && warrior.look[2].to_s != "Wizard"
+      backcheck(warrior)
+    elsif warrior.look[1].to_s == "Wizard" && warrior.look[0].to_s != "Captive"
+      warrior.shoot!
+    elsif warrior.look[0].to_s != "Captive" && warrior.look[1].to_s != "Captive" && warrior.look[2].to_s == "Wizard"
+      warrior.shoot!
+    elsif warrior.look[2].to_s == "Archer" && warrior.look[1].to_s == "Thick Sludge"
+      warrior.shoot!
+    else
+      backcheck(warrior)
+    end #end if
+  end #end def
+
+
+  def backcheck(warrior) #makes warrior check behind him for enemies and allies before continuing forward
+    if warrior.feel(:backward).wall? == false && $forward != 1 &&
+      if warrior.feel(:backward).empty?
+        warrior.walk!(:backward)
+      elsif warrior.feel(:backward).captive?
+        warrior.rescue!(:backward)
+      end #ends inner-if
+    elsif warrior.look[2].to_s == "Archer" && warrior.health < 10
+      warrior.walk!(:backward)
+    else
+        forward_move(warrior)
+    end #ends outer-if
+  end #ends def backcheck
+
+  def forward_move(warrior)
+    if warrior.feel(:backward).wall? == true or $forward == 1 #if warrior has reached the back or has already started moving forward
+      $forward = 1
+      if warrior.feel.empty?
+        warrior.walk!
+      elsif warrior.feel.captive?
+        warrior.rescue!
+      else
+        warrior.attack!
+      end #ends inner-if
+    end #ends if
+  end #ends def forward_move
+
+end #ends class Player
